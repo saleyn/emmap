3b3373ee13c0713a16ac233ddefe102d35863672
Serge Aleynikov <saleyn@gmail.com>
2022-08-22 22:28:53 -0400

    Add queue peek

--- src/emmap_queue.erl
+++ src/emmap_queue.erl
@@ -14,10 +14,11 @@
 %%%-----------------------------------------------------------------------------
 -module(emmap_queue).
 -export([open/3, close/1]).
--export([purge_queue/1, is_empty/1, length/1, push/2, pop/1, pop_and_purge/1]).
+-export([purge_queue/1, is_empty/1, length/1, push/2, pop/1, peek/1, try_pop/2,
+         pop_and_purge/1, try_pop_and_purge/2]).
 
 -export([start_link/4, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
--export([enqueue/2, dequeue/1, info/1]).
+-export([enqueue/2, dequeue/1, try_dequeue/2, inspect/1, info/1]).
 
 -compile({no_auto_import,[length/1]}).
 
@@ -41,21 +42,48 @@
   , segm_sz  :: non_neg_integer()
 }).
 
+-define(TIMEOUT, infinity).
+
 %%------------------------------------------------------------------------------
 %% Gen_server API
 %%------------------------------------------------------------------------------
 
+%% @doc Start a queue process.
+%% `Name' - name of the registered process.
+%% `Filename' - name of the memory mapped file.
+%% `SegmSize' - size of the memory mapped segment (can be 0 for an existing file).
+%% `Opts' - see `emmap:open_options()'.
 start_link(Name, Filename, SegmSize, Opts) when is_list(Opts) ->
-  gen_server:start_link(Name, [Filename, SegmSize, Opts], []).
+  gen_server:start_link({local, Name}, ?MODULE, [Filename, SegmSize, Opts], []).
 
+%% @doc Enqueue a message to the queue
 enqueue(Name, Term) ->
-  gen_server:call(Name, {push, Term}, infinity).
+  gen_server:call(Name, {push, Term}, ?TIMEOUT).
 
+%% @doc Dequeue a message from the queue
 dequeue(Name) ->
-  gen_server:call(Name, pop, infinity).
+  gen_server:call(Name, pop, ?TIMEOUT).
 
+%% @doc Dequeue a message from the queue.
+%% The function returns `Res' where `Res' is the result of evaluating
+%% the `Fun' with the poped element from the queue.  If the `Fun' throws an
+%% exception, the exception if propagated to the caller.
+try_dequeue(Name, Fun) when is_function(Fun, 1) ->
+  case gen_server:call(Name, {try_pop, Fun}, ?TIMEOUT) of
+    {ok, Res} ->
+      Res;
+    {error, Error, Stacktrace} ->
+      erlang:error(Error, Stacktrace)
+  end.
+
+%% @doc Peek a message from the queue without dequeuing it
+inspect(Name) ->
+  gen_server:call(Name, peek, ?TIMEOUT).
+
+%% @doc Return queue info as a map
+-spec info(atom()) -> map().
 info(Name) ->
-  gen_server:call(Name, info, infinity).
+  gen_server:call(Name, info, ?TIMEOUT).
 
 %%------------------------------------------------------------------------------
 %% Gen_server callbacks
@@ -75,12 +103,18 @@ handle_call({push, Term}, _From, #state{mem=Mem} = State) ->
   end;
 
 handle_call(pop, _From, #state{mem=Mem} = State) ->
-  case pop_and_purge(Mem) of
-    nil ->
-      %% Empty queue
-      {reply, nil, State};
-    Msg ->
-      {reply, Msg, State}
+  Msg = pop_and_purge(Mem),
+  {reply, Msg, State};
+
+handle_call(peek, _From, #state{mem=Mem} = State) ->
+  Msg = peek(Mem),
+  {reply, Msg, State};
+
+handle_call({try_pop, Fun}, _From, #state{mem=Mem} = State) ->
+  try
+    {reply, {ok, try_pop_and_purge(Mem, Fun)}, State}
+  catch _:E:ST ->
+    {reply, {error, E, ST}, State}
   end;
 
 handle_call(info, _From, #state{mem=Mem} = State) ->
@@ -104,6 +138,7 @@ terminate(_Reason, #state{mem=Mem}) ->
 %% Raw segment access functions
 %%------------------------------------------------------------------------------
 
+%% @doc Open a memory mapped queue.
 open(Filename, Size, Opts) when is_list(Filename), is_integer(Size), is_list(Opts) ->
   ok = filelib:ensure_dir(filename:dirname(Filename)),
   case emmap:open(Filename, 0, Size, [create, read, write | Opts]) of
@@ -127,11 +162,13 @@ open(Filename, Size, Opts) when is_list(Filename), is_integer(Size), is_list(Opt
       Error
   end.
 
+%% @doc Close a previously open queue.
 close(Mem) ->
   emmap:close(Mem).
 
+%% @doc Purge queue.  It is a constant time operation.
 purge_queue(Mem) ->
-  case is_empty_segment(Mem) of
+  case is_empty_queue(Mem) of
     {true, 0} ->
       true;
     {true, _} ->
@@ -149,22 +186,26 @@ header(Mem) ->
       Error
   end.
 
+%% @doc Returns `true' if the queue is empty.
 is_empty(Mem) ->
-  {Res, _Head} = is_empty_segment(Mem),
-  Res.
+  case is_empty_queue(Mem) of
+    {true, _Head} -> true;
+    false         -> false
+  end.
 
-is_empty_segment(#file_descriptor{} = Mem) ->
+is_empty_queue(#file_descriptor{} = Mem) ->
   case emmap:pread(Mem, 0, 16) of
     {ok, Header} ->
-      is_empty_segment(Header);
+      is_empty_queue(Header);
     {error, Error} ->
       erlang:error(Error)
   end;
-is_empty_segment(<<_Sz:4/binary, H:32/integer, H:32/integer, _/binary>>) ->
+is_empty_queue(<<_Sz:4/binary, H:32/integer, H:32/integer, _/binary>>) ->
   {true, H};
-is_empty_segment(_Header) ->
+is_empty_queue(_Header) ->
   false.
 
+%% @doc Get queue length. This function has a linear-time complexity.
 length(Mem) ->
   F = fun
     G(H, T, Count) when H < T ->
@@ -176,6 +217,7 @@ length(Mem) ->
   {Head, Tail, _, _} = header(Mem),
   F(Head, Tail, 0).
 
+%% @doc Push a term to the queue. This function has a constant-time complexity.
 push(Mem, Term) ->
   Bin = term_to_binary(Term),
   Sz0 = byte_size(Bin)+8,
@@ -204,16 +246,56 @@ push(Mem, Term) ->
        Error
   end)(Arg).
 
+%% @doc Pop a term from the queue. This function has a constant-time complexity.
 pop(Mem) ->
-  case pop_internal(Mem) of
-    {Msg, _IsLastdt} ->
+  case pop_internal(Mem, true) of
+    {Msg, _IsEnd} ->
       Msg;
     nil ->
       nil
   end.
 
+%% @doc Evaluate the `Fun' function on the next term in the queue.
+%% If the function doesn't raise an exception, pop the term from the queue, otherwise
+%% leave the queue unmodified. This function has a constant-time complexity. It returns
+%% the result of evaluating `Fun'.
+try_pop(Mem, Fun) when is_function(Fun, 1) ->
+  case pop_internal(Mem, Fun) of
+    {Res, _IsEnd} ->
+      Res;
+    nil ->
+      nil
+  end.
+
+%% @doc Evaluate the `Fun' function on the next term in the queue.
+%% If the function doesn't raise an exception, pop the term from the queue, otherwise
+%% leave the queue unmodified. This function has a constant-time complexity. It returns
+%% the result of evaluating `Fun'.
+try_pop_and_purge(Mem, Fun) when is_function(Fun, 1) ->
+  case pop_internal(Mem, Fun) of
+    {Res, true} ->
+      ok = emmap:pwrite(Mem, 4, ?INIT_SEGM_HEADER),
+      Res;
+    {Res, false} ->
+      Res;
+    nil ->
+      nil
+  end.
+
+%% @doc Peek the next term from the queue without popping it.
+%% This function has a constant-time complexity.
+peek(Mem) ->
+  case pop_internal(Mem, false) of
+    {Msg, _IsEnd} ->
+      Msg;
+    nil ->
+      nil
+  end.
+
+%% @doc Pop a term from the queue and reclaim queue's memory if the queue is empty.
+%% This function has a constant-time complexity.
 pop_and_purge(Mem) ->
-  case pop_internal(Mem) of
+  case pop_internal(Mem, true) of
     {Msg, true} ->
       ok = emmap:pwrite(Mem, 4, ?INIT_SEGM_HEADER),
       Msg;
@@ -223,7 +305,7 @@ pop_and_purge(Mem) ->
       nil
   end.
 
-pop_internal(Mem) ->
+pop_internal(Mem, Pop) ->
   case header(Mem) of
     {Head,  Tail, NextTail, _SegmSize} when Head < Tail, Head >= ?HEADER_SZ, Tail >= ?HEADER_SZ ->
       {ok,   <<Sz:32/integer>>} = emmap:pread(Mem, Head, 4),
@@ -231,12 +313,23 @@ pop_internal(Mem) ->
       {ok,   <<Bin:BinSz/binary, EndSz:32/integer>>} = emmap:pread(Mem, Head+4, BinSz+4),
       EndSz /= Sz   andalso erlang:error({message_size_mismatch, {Sz, EndSz}, Head}),
       NextHd = Head+Sz,
-      ok     = emmap:pwrite(Mem, 4, <<NextHd:32/integer>>),
-      {binary_to_term(Bin), (NextHd =:= Tail) and (NextHd =:= NextTail)};
+      Msg    = binary_to_term(Bin),
+      IsEnd  = (NextHd =:= Tail) andalso (NextHd =:= NextTail),
+      case Pop of
+        true ->
+          ok = emmap:pwrite(Mem, 4, <<NextHd:32/integer>>),
+          {Msg, IsEnd};
+        false ->
+          {Msg, IsEnd};
+        _ when is_function(Pop, 1) ->
+          Res = Pop(Msg),
+          ok = emmap:pwrite(Mem, 4, <<NextHd:32/integer>>),
+          {Res, IsEnd}
+      end;
     {Head, Tail, _NextTail, _} when Head >= ?HEADER_SZ, Tail >= ?HEADER_SZ ->
       nil;
     {_Head, _Tail, _NextTail, _} = H ->
-      {error, {invalid_queue_header, H}};
+      erlang:error({invalid_queue_header, H});
     {error, Error} ->
       erlang:error(Error)
   end.
@@ -259,13 +352,16 @@ spsc_queue_test() ->
   ?assertEqual(6, length(Mem)),
 
   % Dequeue data
-  ?assertMatch({a, false}, pop_internal(Mem)),
+  ?assertMatch({a, false}, pop_internal(Mem, true)),
   ?assertEqual(b,          pop(Mem)),
   ?assertEqual(c,          pop(Mem)),
   ?assertEqual(1,          pop(Mem)),
-  ?assertMatch({2, false}, pop_internal(Mem)),
-  ?assertMatch({3, true},  pop_internal(Mem)),
+  ?assertMatch(2,          peek(Mem)),
+  ?assertMatch(2,          try_pop(Mem, fun(2) -> 2 end)),
+  ?assertError(failed,     try_pop(Mem, fun(3) -> erlang:error(failed) end)),
+  ?assertMatch({3, true},  pop_internal(Mem, true)),
   ?assertEqual(nil,        pop(Mem)),
+  ?assertEqual({112,112,112,1024}, header(Mem)),
   ?assert(purge_queue(Mem)),
   ?assert(is_empty(Mem)).
 
@@ -278,8 +374,10 @@ gen_server_queue_test() ->
   enqueue(Pid,  c),
 
   ?assertEqual(a,   dequeue(Pid)),
+  ?assertEqual(b,   inspect(Pid)),
   ?assertEqual(b,   dequeue(Pid)),
-  ?assertEqual(c,   dequeue(Pid)),
+  ?assertError(failed, try_dequeue(Pid, fun(c) -> erlang:error(failed) end)),
+  ?assertEqual(c,   try_dequeue(Pid, fun(c) -> c end)),
   ?assertEqual(nil, dequeue(Pid)),
 
   Blob = list_to_binary(string:copies("x", 256)),
